debea65d6974be049442de30b1f10222985144e0
poulonani <oumbapro@gmail.com>
2026-02-20 18:30:58 +0100

    Removed Secrets From Caching

--- Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net10.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26d61de63e715bf263f9d46ff1728a441b65367")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc0431d7f5500c9e1b63f4c41837b95bb397371")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Tests/obj/Debug/net10.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net10.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e26d61de63e715bf263f9d46ff1728a441b65367")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc0431d7f5500c9e1b63f4c41837b95bb397371")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
